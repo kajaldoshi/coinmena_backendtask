66a322a0d71f1a36fd17db3163d9fc88159114ee
Kajal Doshi <kajjaldoshi@gmail.com>
2021-11-27 23:32:30 +0400

    Hiding the secret key, loading from .env file

--- coinmena/coinmena/settings.py
+++ coinmena/coinmena/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'uov%$_%4mo()61mvcq%jh-j%r-54-)+wzn!t&nev-wwhldmm-5'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
